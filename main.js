2a0959fe7a8be50007db02893a0b7f814f08cb17
laraxx_a <129195275+akebyta@users.noreply.github.com>
2024-04-16 08:59:51 -0300

    add code

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
+const textos= document. querySelectorAll("aba.conteudo");
+ for (let i=0; i < botoes.length; i++) {
+    botoes[1].onclick = function () {
+        for (let j=0; j < botoes.lenght; j++) {
+            botoes{j}.classlist.remove("ativo");
+            textos{j}.classlist.remove("ativo");
         }
-
-        botoes[i].classList.add("ativo");
+        botoes[1].classlist.add("ativo");
+        textos[1].classlist.add("ativo");
     }
-}
+ }
+
