d89acd4af8261159879e445cc7aee162844de7d6
laraxx_a <129195275+akebyta@users.noreply.github.com>
2024-04-30 08:19:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
-const textos= document. querySelectorAll("aba.conteudo");
- for (let i=0; i < botoes.length; i++) {
-    botoes[1].onclick = function () {
-        for (let j=0; j < botoes.lenght; j++) {
-            botoes{j}.classlist.remove("ativo");
-            textos{j}.classlist.remove("ativo");
-        }
-        botoes[i].classlist.add("ativo");
-        textos[i].classlist.add("ativo");
-    }
- }
+const textos = document.querySelectorAll(".aba-conteudo");
 
+for (let i = 0; i < botoes.length; i++) {
+botoes[i].onclick = function () {
+
+for (let j = 0; j < botoes.length; j++) {
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+}
+
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+}
+}
